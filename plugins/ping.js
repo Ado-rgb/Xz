18082d4cc3eb417b37aa66726cf4dfe1476ffcbe
Ado <minexdt@gmail.com>
2025-08-10 10:57:15 -0600

    Delete plugins directory

--- plugins/ping.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: 'ping',
-  description: 'Responde pong',
-  execute: async (sock, msg, conn) => {
-    await sock.sendMessage(msg.key.remoteJid, { text: '🏓 Pong!' }, { quoted: msg });
-  }
-};
