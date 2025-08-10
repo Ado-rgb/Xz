74a9c71047f837746d88ee047ae6f0680cd807f1
Ado <minexdt@gmail.com>
2025-08-10 11:28:21 -0600

    Delete public directory

--- public/terminal.js
+++
@@ -1,7 +0,0 @@
-const term = new Terminal();
-term.open(document.getElementById('terminal'));
-
-const socket = new WebSocket(`ws://${location.host}`);
-
-term.onData(data => socket.send(data));
-socket.onmessage = e => term.write(e.data);
